08143bea0dda575f40f3b60a72f5d4b56d508c50
Staryy-Dobryy <Lfcega1@gmail.com>
2025-04-09 23:45:49 +0300

    Create new exceptions

---
+++ src/ParquetMapper/Exceptions/IncompatibleSchemaTypeException.cs
@@ -0,0 +1,18 @@
+﻿using Parquet.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParquetMapper.Exceptions
+{
+    public class IncompatibleSchemaTypeException : Exception
+    {
+        public readonly ParquetSchema parquetSchema;
+        public IncompatibleSchemaTypeException(ParquetSchema parquetSchema, Type type) : base($"Invalid {type} for {parquetSchema}") 
+        {
+            this.parquetSchema = parquetSchema;
+        }
+    }
+}
---
+++ src/ParquetMapper/Exceptions/OutOfRowGroupRangeException.cs
@@ -0,0 +1,18 @@
+﻿using ParquetMapper.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParquetMapper.Exceptions
+{
+    public class OutOfRowGroupRangeException : Exception
+    {
+        public readonly ParquetData<object> parquetData;
+        public OutOfRowGroupRangeException(object data) : base($"Invalid row group index for the {data}")
+        {
+            parquetData = (ParquetData<object>?)data;
+        }
+    }
+}
