9f5fe7240115e45741953e388284bdae597c1c17
Staryy-Dobryy <Lfcega1@gmail.com>
2025-04-09 23:45:06 +0300

    Create new attribute

---
+++ src/ParquetMapper/Attributes/IgnoreCasingAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParquetMapper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class IgnoreCasingAttribute : Attribute
+    {
+    }
+}
