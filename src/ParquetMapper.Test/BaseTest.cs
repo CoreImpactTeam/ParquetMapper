f8ec6fc96f7a38d8b9053119d0d0da3364379207
Staryy-Dobryy <Lfcega1@gmail.com>
2025-04-19 23:31:39 +0300

    Update base class fo tests

--- src/ParquetMapper.Test/BaseTest.cs
+++ src/ParquetMapper.Test/BaseTest.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit.Sdk;
@@ -41,16 +42,14 @@ namespace ParquetMapper.Test
         }
         protected async Task<ParquetData<TDataType>> ReadParquetAsync<TDataType>(string path, CancellationToken cancellationToken = default) where TDataType : new()
         {
-            // Открываем поток для чтения файла Parquet
             using (Stream fileStream = File.OpenRead(path))
             {
                 using (var parquetReader = await ParquetReader.CreateAsync(fileStream))
                 {
-                    Stopwatch sw = new();
 
                     var dict = parquetReader.Schema.CompareSchema<TDataType>();
 
-                    if (dict != null)
+                    if (dict.Count != 0)
                     {
                         TDataType[][] result = new TDataType[parquetReader.RowGroupCount][];
 
@@ -90,6 +89,52 @@ namespace ParquetMapper.Test
 
             }
         }
+        protected async IAsyncEnumerable<TDataType[]> ReadParquetAsAsyncEnumerable<TDataType>(string path, [EnumeratorCancellation]CancellationToken cancellationToken = default) where TDataType : new()
+        {
+            using (Stream fileStream = File.OpenRead(path))
+            {
+                using (var parquetReader = await ParquetReader.CreateAsync(fileStream))
+                {
+
+                    var dict = parquetReader.Schema.CompareSchema<TDataType>();
+
+                    if (dict.Count == 0)
+                    {
+                        throw new IncompatibleSchemaTypeException(parquetReader.Schema, typeof(TDataType));
+                    }
+
+                    long rowsCount = parquetReader.Metadata.NumRows / parquetReader.RowGroupCount;
+                    TDataType[] result = new TDataType[rowsCount];
+
+                    for (int i = 0; i < rowsCount; i++)
+                    {
+                        result[i] = new();
+                    }
+
+                    for (int i = 0; i < parquetReader.RowGroupCount; i++)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+
+                        var rowGroup = await parquetReader.ReadEntireRowGroupAsync(i);
+
+                        int rowGroupLength = rowGroup.First().Data.Length;
+
+                        for (int row = 0; row < rowGroupLength; row++)
+                        {
+                            foreach (var column in rowGroup)
+                            {
+                                if (dict.TryGetValue(column.Field.Name, out var prop))
+                                {
+                                    prop.SetValue(result[row], column.Data.GetValue(row));
+                                }
+                            }
+                        }
+
+                        yield return result.Take(rowGroupLength).ToArray();
+                    }
+                }
+            }
+        }
         private string HandleAttributes(IEnumerable<Attribute>? attributes, string propertyName)
         {
             if (attributes == null)
@@ -109,7 +154,6 @@ namespace ParquetMapper.Test
                     case HasParquetColNameAttribute temp:
                         if (string.IsNullOrEmpty(temp.ColName))
                         {
-                            // TODO: create 'NullColNameException'
                             throw new NullColNameException();
                         }
 
