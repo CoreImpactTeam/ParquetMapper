77b0b385785a95c98e5e66ae0c9665578f617aea
Staryy-Dobryy <Lfcega1@gmail.com>
2025-04-25 00:01:35 +0300

    Add docx for AttributeTransformContext class

--- src/ParquetMapper/Abstractions/Mapping/RowGroupWriterBase.cs
+++ src/ParquetMapper/Abstractions/Mapping/RowGroupWriterBase.cs
@@ -1,6 +1,7 @@
 ﻿using Parquet;
 using Parquet.Data;
 using Parquet.Schema;
+using ParquetMapper.Attributes.Processing.AttributeTransformContext;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
@@ -26,9 +27,40 @@ namespace ParquetMapper.Abstractions.Mapping
         private static readonly ConcurrentDictionary<Type, TypeMetadata> _typeMetadataCache = new();
 
         /// <summary>
+        /// A cache of attribute transformation contexts, where the key is the <see cref="Type"/> of the object,
+        /// and the value is the <see cref="AttributeTransformContext"/> for that type.
+        /// The cache is static and thread-safe for sharing across all instances of the class.
+        /// </summary>
+        private static readonly ConcurrentDictionary<Type, AttributeTransformContext> _typeTransformCache = new();
+
+        /// <summary>
+        /// Retrieves or creates an <see cref="AttributeTransformContext"/> for the specified type <typeparamref name="T"/>.
+        /// If a context for this type already exists in the cache, it is returned; otherwise, a new one is created and cached.
+        /// </summary>
+        /// <typeparam name="T">The type for which to retrieve or create the transform context.</typeparam>
+        /// <returns>An <see cref="AttributeTransformContext"/> instance for the specified type.</returns>
+        protected AttributeTransformContext GetOrCreateTransformContext<T>()
+        {
+            Type type = typeof(T);
+
+            return GetOrCreateTransformContext(type);
+        }
+
+        /// <summary>
+        /// Retrieves or creates an <see cref="AttributeTransformContext"/> for the specified <see cref="Type"/>.
+        /// If a context for this type already exists in the cache, it is returned; otherwise, a new one is created and cached.
+        /// </summary>
+        /// <param name="type">The type for which to retrieve or create the transform context.</param>
+        /// <returns>An <see cref="AttributeTransformContext"/> instance for the specified type.</returns>
+        protected AttributeTransformContext GetOrCreateTransformContext(Type type)
+        {
+            return _typeTransformCache.GetOrAdd(type, _ => new AttributeTransformContext(type));
+        }
+
+        /// <summary>
         /// Retrieves the metadata for the specified type <typeparamref name="T"/>.
         /// If the metadata for this type already exists in the cache, it is returned from the cache.
-        /// Otherwise, new metadata is created using <see cref="CreateTypeMetadata{T}(ParquetSchema)"/>,
+        /// Otherwise, new metadata is created using <see cref="CreateTypeMetadata(ParquetSchema, Type)"/>,
         /// added to the cache, and then returned.
         /// </summary>
         /// <typeparam name="T">The type of objects for which to retrieve metadata.</typeparam>
@@ -37,39 +69,57 @@ namespace ParquetMapper.Abstractions.Mapping
         protected TypeMetadata GetOrCreateMetadata<T>(ParquetSchema schema)
         {
             Type type = typeof(T);
-            return _typeMetadataCache.GetOrAdd(type, _ => CreateTypeMetadata<T>(schema));
+
+            return GetOrCreateMetadata(schema, type);
         }
 
         /// <summary>
-        /// Creates the type metadata for <typeparamref name="T"/> to be written to Parquet.
+        /// Retrieves the metadata for the specified <see cref="Type"/>.
+        /// If the metadata for this type already exists in the cache, it is returned from the cache.
+        /// Otherwise, new metadata is created using <see cref="CreateTypeMetadata(ParquetSchema, Type)"/>,
+        /// added to the cache, and then returned.
+        /// </summary>
+        /// <param name="schema">The Parquet schema that describes the data structure.</param>
+        /// <param name="type">The type of objects for which to retrieve metadata.</param>
+        /// <returns>A <see cref="TypeMetadata"/> instance containing the metadata for the specified <paramref name="type"/>.</returns>
+        protected TypeMetadata GetOrCreateMetadata(ParquetSchema schema, Type type)
+        {
+            return _typeMetadataCache.GetOrAdd(type, _ => CreateTypeMetadata(schema, type));
+        }
+
+        /// <summary>
+        /// Creates the type metadata for a given <see cref="Type"/> to be written to Parquet.
         /// This includes obtaining property information, creating getters for fast access to values,
         /// and mapping property names to fields in the Parquet schema.
         /// </summary>
-        /// <typeparam name="T">The type of objects for which to create metadata.</typeparam>
         /// <param name="schema">The Parquet schema that describes the data structure.</param>
+        /// <param name="type">The type of objects for which to create metadata.</param>
         /// <returns>A <see cref="TypeMetadata"/> instance containing the created metadata.</returns>
-        private TypeMetadata CreateTypeMetadata<T>(ParquetSchema schema)
+        private TypeMetadata CreateTypeMetadata(ParquetSchema schema, Type type)
         {
-            PropertyInfo[] properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            AttributeTransformContext transformContext = GetOrCreateTransformContext(type);
+
+            PropertyInfo[] properties = transformContext.Properties as PropertyInfo[] ?? transformContext.Properties.ToArray();
 
             Func<object, object>[] getters = properties.Select(property =>
             {
                 ParameterExpression parameter = Expression.Parameter(typeof(object), "instance");
-                Expression castInstance = Expression.Convert(parameter, typeof(T));
+                Expression castInstance = Expression.Convert(parameter, type);
                 Expression propertyAccess = Expression.Property(castInstance, property);
                 Expression convertToObject = Expression.Convert(propertyAccess, typeof(object));
                 return Expression.Lambda<Func<object, object>>(convertToObject, parameter).Compile();
             }).ToArray();
 
             Dictionary<string, DataField> dataFields = properties.ToDictionary(
-                property => property.Name,
+                property => transformContext.TransformTextByPropertyAttributes(property, property.Name),
                 property => schema.FindDataField(property.Name));
 
             return new TypeMetadata(properties, getters, dataFields);
         }
 
         /// <summary>
-        /// Asynchronously writes a batch of objects as a row group to the specified <see cref="ParquetWriter"/>.
+        /// Asynchronously writes a batch of objects of type <typeparamref name="T"/> as a row group
+        /// to the specified <see cref="ParquetWriter"/>.
         /// Uses the provided <see cref="ParquetSchema"/> to determine the data structure.
         /// The metadata for the type <typeparamref name="T"/> is retrieved or created using
         /// <see cref="GetOrCreateMetadata{T}(ParquetSchema)"/>.
@@ -117,6 +167,5 @@ namespace ParquetMapper.Abstractions.Mapping
             PropertyInfo[] Properties,
             Func<object, object>[] Getters,
             Dictionary<string, DataField> DataFields);
-
     }
 }
