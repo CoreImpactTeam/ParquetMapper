cefa3f9aca609c882ca54e2facd158ddd0e09ffc
Staryy-Dobryy <Lfcega1@gmail.com>
2025-04-27 23:59:23 +0300

    Add new exception

---
+++ src/ParquetMapper/Exceptions/FailedToReadRowGroupException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParquetMapper.Exceptions
+{
+    public class FailedToReadRowGroupException : Exception
+    {
+        public FailedToReadRowGroupException(int rowGroupIndex) : base($"Reading failed on index {rowGroupIndex}") { }
+    }
+}
